ff02f93a064e5939e8ae75956ba0a32d76bccd51
Quentin Desgrippes <desgrippes.quentin@gmail.com>
2024-12-10 21:53:22 +0100

    adding controllers and models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_10_195345) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_10_202456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.integer "km"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
